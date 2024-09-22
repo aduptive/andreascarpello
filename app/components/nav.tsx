c32964256c6d38685e5c99ce6aeee1429de351df
Andrea Scarpello <aduptive@gmail.com>
2024-09-21 21:52:14 -0300

    feat: setup

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,21 +1,21 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const navItems = {
-  '/': {
-    name: 'home',
+  "/blog": {
+    name: "blog",
   },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
-}
+};
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
+    <header className="sticky top-0">
+      <div className="container mx-auto flex items-center">
+        <div className="flex flex-row justify-between items-center py-4">
+          <div className="flex flex-row items-center">
+            <Link href="/">Andrea Scarpello</Link>
+          </div>
+        </div>
+
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
@@ -30,11 +30,11 @@ export function Navbar() {
                 >
                   {name}
                 </Link>
-              )
+              );
             })}
           </div>
         </nav>
       </div>
-    </aside>
-  )
+    </header>
+  );
 }
